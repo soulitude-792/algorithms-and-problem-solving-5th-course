4516c0b011c26fc3edaafced44fe19bebcd41f6f
Ibrahim <ngc62055@gmail.com>
2026-07-01 23:37:54 +0300

    Problem 26

---
+++ Problem 26/Problem 26.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<cstdlib>
+
+int RandomNumber(int From, int To)
+{
+	int RandomNumber{ rand() % (To - From + 1) + From };
+
+	return RandomNumber;
+}
+
+int ReadPositiveNumber(std::string Message)
+{
+	int Num;
+	do
+	{
+		std::cout << Message;
+		std::cin >> Num;
+	} while (Num <= 0 || Num > 100);
+
+	return Num;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int length)
+{
+	for (int i{ 0 }; i < length; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+
+void PrintArray(int Length, int arr[100])
+{
+	for (int i{ 0 }; i < Length; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << '\n';
+}
+
+int FindSumArray(int arr[100], int length)
+{
+	int Sum{ 0 };
+
+	for (int i{ 0 }; i < length; i++)
+	{
+		Sum += arr[i];
+	}
+
+	return Sum;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100];
+	int length;
+	int SumArray;
+
+	length = ReadPositiveNumber("Enter the length of the array (100 max): ");
+
+	FillArrayWithRandomNumbers(arr, length);
+
+
+	std::cout << "Array elements: ";
+	PrintArray(length, arr);
+
+	SumArray = FindSumArray(arr, length);
+	std::cout << "\nThe sum of your array is: " << SumArray << '\n';
+
+
+	return 0;
+}
