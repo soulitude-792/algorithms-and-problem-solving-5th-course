6f8f88a2c6b288a042e169dcf208f06cb74dee64
Ibrahim <ngc62055@gmail.com>
2026-07-01 23:55:46 +0300

    Problem 27

---
+++ Problem 27/Problem 27.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<cstdlib>
+
+int RandomNumber(int From, int To)
+{
+	int RandomNumber{ rand() % (To - From + 1) + From };
+
+	return RandomNumber;
+}
+
+int ReadPositiveNumber(std::string Message)
+{
+	int Num;
+	do
+	{
+		std::cout << Message;
+		std::cin >> Num;
+	} while (Num <= 0 || Num > 100);
+
+	return Num;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int &length)
+{
+	for (int i{ 0 }; i < length; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+
+void PrintArray(int Length, int arr[100])
+{
+	for (int i{ 0 }; i < Length; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << '\n';
+}
+
+int FindSumArray(int arr[100], int length)
+{
+	int Sum{ 0 };
+
+	for (int i{ 0 }; i < length; i++)
+	{
+		Sum += arr[i];
+	}
+	
+
+	return Sum;
+}
+
+float FindAvgArray(int arr[100], int length)
+{
+
+	return (float) FindSumArray(arr, length) / length;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100];
+	int length;
+
+	length = ReadPositiveNumber("Enter the length of the array (100 max): ");
+
+	FillArrayWithRandomNumbers(arr, length);
+
+
+	std::cout << "Array elements: ";
+	PrintArray(length, arr);
+	
+	std::cout << "\nThe average number of your array is: " << FindAvgArray(arr, length) << '\n';
+
+
+	return 0;
+}
