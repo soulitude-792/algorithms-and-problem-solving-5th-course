375ac00a189ce83342942b6afeed2132c8f24b43
Ibrahim <ngc62055@gmail.com>
2026-08-22 23:34:52 +0300

    hello

---
+++ Problem 38/Problem 38.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cstdlib>
+
+int ReadPositiveNumber(std::string Message)
+{
+	int Num;
+	do
+	{
+		std::cout << Message;
+		std::cin >> Num;
+	} while (Num <= 0 || Num > 100);
+
+	return Num;
+}
+
+int RandomNumber(int From, int To)
+{
+	int RandomNumber{ rand() % (To - From + 1) + From };
+
+	return RandomNumber;
+}
+
+void AddArrayNumber(int element, int arr2[100], int& length)
+{
+	length++;
+	arr2[length - 1] = element;
+}
+
+void CopyArrayUsingAddArrayNumber(int arr1[100], int arr2[100], int arrlen1, int& arrlen2)
+{
+	for (int i = 0; i < arrlen1; i++)
+	{
+		if (arr1[i] % 2 == 1)
+		{
+			AddArrayNumber(arr1[i], arr2, arrlen2);
+		}
+	}
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int length)
+{
+	for (int i{ 0 }; i < length; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+
+void PrintArray(int Length, int arr[100])
+{
+
+	for (int i{ 0 }; i < Length; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << '\n';
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr1[100], arr2[100];
+
+	int arrLength1 = 0;
+	int arrLength2 = 0;
+
+	arrLength1 = ReadPositiveNumber("Enter the length of the array: ");
+
+	FillArrayWithRandomNumbers(arr1, arrLength1);
+
+	CopyArrayUsingAddArrayNumber(arr1, arr2, arrLength1, arrLength2);
+
+	std::cout << "\nArray 1 elements: ";
+	PrintArray(arrLength1, arr1);
+
+	std::cout << "Array 2 Odd elements: ";
+	PrintArray(arrLength2, arr2);
+
+	return 0;
+
+}
