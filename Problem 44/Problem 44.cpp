9f7858af4b539f675363406412d58808d86fc597
Ibrahim <ngc62055@gmail.com>
2026-08-27 01:24:58 +0300

    Problem 44

---
+++ Problem 44/Problem 44.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<cstdlib>
+
+int RandomNumber(int From, int To)
+{
+	int RandomNumber{ rand() % (To - From + 1) + From };
+
+	return RandomNumber;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& length)
+{
+	std::cout << "Enter the number of elements: ";
+	std::cin >> length;
+
+	for (int i{ 0 }; i < length; i++)
+	{
+		arr[i] = RandomNumber(-100, 100);
+	}
+}
+
+void PrintArray(int Length, int arr[100])
+{
+
+	for (int i{ 0 }; i < Length; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << '\n';
+}
+
+int  PositiveNumberCountInArray(int arr[100], int arrlen)
+{
+	int PositiveNumberCount = 0;
+
+	for (int i = 0; i < arrlen; i++)
+	{
+		if (arr[i] > 0)
+		{
+			PositiveNumberCount++;
+		}
+	}
+	return PositiveNumberCount;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100];
+	int length = 0;
+
+	FillArrayWithRandomNumbers(arr, length);
+
+	std::cout << "\nArray elements: ";
+	PrintArray(length, arr);
+
+	std::cout << "Positive number count is: " << PositiveNumberCountInArray(arr, length) << '\n';
+	
+	return 0;
+}
+
+/*the purpose of the program is to fill the array with random numbers and then count only the Positive 
+numbers in the array and return the number of the count. it does this by filling the array with random
+numbers first using FillArrayWithRandomNumbers and then it sends that array with the length to a function
+called PositiveNumberCountInArray. then it checks every element if it's Postive or Negative, if it's positive
+it adds 1 to the counter and loops until the end*/
