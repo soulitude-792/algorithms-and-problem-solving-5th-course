bc68eeacfbe159912a7fa340b1bb3cad9af8b6cd
Ibrahim <ngc62055@gmail.com>
2026-08-27 01:13:19 +0300

    Problem 43

---
+++ Problem 43/Problem 43.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<cstdlib>
+
+int RandomNumber(int From, int To)
+{
+	int RandomNumber{ rand() % (To - From + 1) + From };
+
+	return RandomNumber;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& length)
+{
+	std::cout << "Enter the number of elements: ";
+	std::cin >> length;
+
+	for (int i{ 0 }; i < length; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+
+void PrintArray(int Length, int arr[100])
+{
+
+	for (int i{ 0 }; i < Length; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << '\n';
+}
+
+int  EvenNumberCountInArray(int arr[100], int arrlen)
+{
+	int EvenNumberCount = 0;
+
+	for (int i = 0; i < arrlen; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			EvenNumberCount++;
+		}
+	}
+	return EvenNumberCount;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100];
+	int length = 0;
+
+	FillArrayWithRandomNumbers(arr, length);
+
+	std::cout << "\nArray elements: ";
+	PrintArray(length, arr);
+
+	std::cout << "Even numbers count is: " << EvenNumberCountInArray(arr, length) << '\n';
+}
+
+/*the purpose of the program is to fill the array with random numbers and then count only the Even numbers
+in the array and return the number of the count. it does this by filling the array with random numbers first
+using FillArrayWithRandomNumbers and then it sends that array with the length to a function called
+EvenNumberCountInArray. then it checks every element if it's odd or even, if it's Even it adds 1 to the 
+counter and loops until the end*/
