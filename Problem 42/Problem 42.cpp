e5f4764bc943fb9dce31a417bed5b8fb44c742d6
Ibrahim <ngc62055@gmail.com>
2026-08-27 01:06:32 +0300

    Problem 42

---
+++ Problem 42/Problem 42.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<cstdlib>
+
+int RandomNumber(int From, int To)
+{
+	int RandomNumber{ rand() % (To - From + 1) + From };
+
+	return RandomNumber;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int &length)
+{
+	std::cout << "Enter the number of elements: ";
+	std::cin >> length;
+
+	for (int i{ 0 }; i < length; i++)
+	{
+		arr[i] = RandomNumber(1, 100);
+	}
+}
+
+void PrintArray(int Length, int arr[100])
+{
+
+	for (int i{ 0 }; i < Length; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << '\n';
+}
+
+int  OddNumberCountInArray(int arr[100], int arrlen)
+{
+	int OddNumberCount = 0;
+
+	for (int i = 0; i < arrlen; i++)
+	{
+		if (arr[i] % 2 != 0)
+		{
+			OddNumberCount++;
+		}
+	}
+	return OddNumberCount;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int arr[100];
+	int length = 0;
+
+	FillArrayWithRandomNumbers(arr, length);
+
+	std::cout << "\nArray elements: ";
+	PrintArray(length, arr);
+
+	std::cout << "Odd numbers count is: " << OddNumberCountInArray(arr, length) << '\n';
+}
+
+/**/
