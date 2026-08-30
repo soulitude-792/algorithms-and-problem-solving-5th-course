8eaaa462ec29c15013173386f577a1d9601523b4
soulitude-792 <ngc62055@gmail.com>
2026-08-31 02:14:37 +0300

    Problem 47

---
+++ Problem 47/Problem 47/Problem 47.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+
+float GetFractionPart(float Number)
+{
+	return (int(Number) - Number);
+}
+
+int MyRound(float Number)
+{
+	int IntPart;
+	IntPart = int(Number);
+
+	float FractionPart = GetFractionPart(Number);
+
+	if (abs(FractionPart) >= 0.5)
+	{
+		if (Number > 0)
+			return ++IntPart;
+		else
+			return --IntPart;
+	}
+	else
+		return IntPart;
+}
+
+float ReadFloatNumber()
+{
+	float Num;
+	std::cout << "enter a float number: ";
+	std::cin >> Num;
+
+	return Num;
+}
+
+int main()
+{
+	float Number = ReadFloatNumber();
+
+	std::cout << "\nMy round function result: " << MyRound(Number);
+	std::cout << "\nOriginal C++ round function result: " << round(Number) << std::endl;
+
+	return 0;
+}
